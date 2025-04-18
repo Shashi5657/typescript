540024cf617bb3d7027c56d90eef21a1529ea7d6
shashi5657 <sangepushashidhar@gmail.com>
2025-04-18 15:23:46 +0530

    added required files

--- react-typescript/src/components/TodoList.tsx
+++ react-typescript/src/components/TodoList.tsx
@@ -8,11 +8,20 @@ interface Props {
 }
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-      ))}
+    <div className="container">
+      <div className="todos">
+
+      </div>
+
+      <div className="todos">
+        
+        </div>
     </div>
+    // <div className="todos">
+    //   {todos.map((todo) => (
+    //     <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+    //   ))}
+    // </div>
   );
 };
 
